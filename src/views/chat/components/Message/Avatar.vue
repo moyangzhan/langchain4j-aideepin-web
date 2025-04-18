a6f83243279055709ff64117a1b9933338e63e4a
ethan_peng <78828540@qq.com>
2025-04-18 15:52:44 +0800

    feat: 添加 siliconflow 平台配置

--- src/views/chat/components/Message/Avatar.vue
+++ src/views/chat/components/Message/Avatar.vue
@@ -9,6 +9,7 @@ import baiduchatgpt from '@/assets/baiduchatgpt.png'
 import tongyiqianwen from '@/assets/tongyiqianwen.png'
 import deepseekAvatar from '@/assets/deepseek.png'
 import openai from '@/assets/openai.png'
+import siliconflow from '@/assets/siliconflow.svg'
 
 interface Props {
   name?: string
@@ -24,17 +25,88 @@ const avatar = computed(() => userStore.userInfo.avatar)
 <template>
   <!-- User's avatar -->
   <template v-if="name === 'user'">
-    <NAvatar v-if="isString(avatar) && avatar.length > 0" :size="imageSize ? imageSize : 32" :src="avatar" :fallback-src="defaultAvatar" :style="{ backgroundColor: 'transparent' }" />
-    <NAvatar v-else round :size="imageSize ? imageSize : 32" :src="defaultAvatar" style="backgroundColor:transparent" />
+    <NAvatar
+      v-if="isString(avatar) && avatar.length > 0"
+      :size="imageSize ? imageSize : 32"
+      :src="avatar"
+      :fallback-src="defaultAvatar"
+      :style="{ backgroundColor: 'transparent' }"
+    />
+    <NAvatar
+      v-else
+      round
+      :size="imageSize ? imageSize : 32"
+      :src="defaultAvatar"
+      style="backgroundcolor: transparent"
+    />
   </template>
   <!-- Model platform's avatar -->
-  <NAvatar v-else-if="name === 'deepseek'" object-fit="contain" round :size="imageSize ? imageSize : 32" :src="deepseekAvatar" :style="{ backgroundColor: 'transparent' }" />
-  <NAvatar v-else-if="name === 'dashscope'" object-fit="contain" round :size="imageSize ? imageSize : 32" :src="tongyiqianwen" :style="{ backgroundColor: 'transparent' }" />
-  <NAvatar v-else-if="name === 'qianfan'" object-fit="contain" round :size="imageSize ? imageSize : 32" :src="baiduchatgpt" :style="{ backgroundColor: 'transparent' }" />
-  <NAvatar v-else-if="name === 'ollama'" object-fit="contain" round :size="imageSize ? imageSize : 32" :src="ollamaAvatar" :style="{ backgroundColor: 'transparent' }" />
-  <NAvatar v-else-if="name === 'openai'" object-fit="contain" round :size="imageSize ? imageSize : 32" :src="openai" :style="{ backgroundColor: 'transparent' }" />
-  <NAvatar v-else-if="name" round bordered :style="{ color: 'gray', border: 'solid 1px gray', backgroundColor: 'transparent' }">
+  <NAvatar
+    v-else-if="name === 'deepseek'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="deepseekAvatar"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name === 'dashscope'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="tongyiqianwen"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name === 'qianfan'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="baiduchatgpt"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name === 'ollama'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="ollamaAvatar"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name === 'openai'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="openai"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name === 'siliconflow'"
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="siliconflow"
+    :style="{ backgroundColor: 'transparent' }"
+  />
+  <NAvatar
+    v-else-if="name"
+    round
+    bordered
+    :style="{
+      color: 'gray',
+      border: 'solid 1px gray',
+      backgroundColor: 'transparent',
+    }"
+  >
     {{ name.substring(0, 2) }}
   </NAvatar>
-  <NAvatar v-else object-fit="contain" round :size="imageSize ? imageSize : 32" :src="openai" :style="{ backgroundColor: 'transparent' }" />
+  <NAvatar
+    v-else
+    object-fit="contain"
+    round
+    :size="imageSize ? imageSize : 32"
+    :src="openai"
+    :style="{ backgroundColor: 'transparent' }"
+  />
 </template>
